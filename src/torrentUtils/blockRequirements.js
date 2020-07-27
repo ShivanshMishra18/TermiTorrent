68477141d0d404ef0f20418ab224808bb43c9478
ShivanshMishra18 <shivansh18@iitg.ac.in>
2020-07-27 17:25:28 +0530

    Some cleanup

--- src/torrentUtils/blockRequirements.js
+++ src/torrentUtils/blockRequirements.js
@@ -1,30 +1,42 @@
-const bencode = require('bencode');
-const fs = require('fs');
-const crypto = require('crypto');
 const bignum = require('bignum');
 
 module.exports.BLOCK_LENGTH = Math.pow(2, 14);
 
+
 // @args - [ torrent(decoded), piece index ]
-// @ret  - [ length of the piece for a given piece index]
-module.exports.findPieceLength = (torrent, pieceIndex) => {
-  const totalLength = bignum.fromBuffer(this.size(torrent)).toNumber();
-  const pieceLength = torrent.info['piece length'];
+// @ret  - [ length of the piece for a given piece index ]
+module.exports.getPieceLength = (torrent, pieceIndex) => {
+    const totalLength = bignum.fromBuffer(this.size(torrent)).toNumber();
+    const pieceLength = torrent.info['piece length'];
 
-  const lastPieceLength = totalLength % pieceLength;
-  const lastPieceIndex = Math.floor(totalLength / pieceLength);
+    const lastPieceLength = totalLength % pieceLength;
+    const lastPieceIndex = Math.floor(totalLength / pieceLength);
 
-  if(lastPieceIndex === pieceIndex)
-	return lastPieceLength;
-  else
-	return pieceLength;  
+    if(lastPieceIndex === pieceIndex)
+        return lastPieceLength;
 
+    return pieceLength;  
 };
+
 
 // @args - [ torrent(decoded), piece index ]
 // @ret  - [ number of blocks for a given piece index ]
-module.exports.findBlocksPerPiece = (torrent, pieceIndex) => {
-  const pieceLength = this.findPieceLength(torrent, pieceIndex);
-	
-  return Math.ceil(pieceLength / this.BLOCK_LENGTH);
+module.exports.getBlocksPerPiece = (torrent, pieceIndex) => {
+    const pieceLength = this.getPieceLength(torrent, pieceIndex);
+    return Math.ceil(pieceLength / this.BLOCK_LENGTH);
+};
+
+
+// @args - [ torrent(decoded), piece index, block index ]
+// @ret  - [ length of the block for a given block index with given piece index ]
+module.exports.getBlockLength = async (torrent, pieceIndex, blockIndex) => {
+    const pieceLength = await this.getPieceLength(torrent, pieceIndex);
+
+    const lastBlockLength = pieceLength % this.BLOCK_LENGTH;
+    const lastBlockIndex = Math.floor(pieceLength / this.BLOCK_LENGTH);
+
+    if(lastBlockIndex === blockIndex)
+        return lastBlockLength;
+
+    return this.BLOCK_LENGTH;  
 };
